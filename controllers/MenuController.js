fa87154f0a87d9eba68fea1a20632b0f39f4f64f
Jayesh Nair <Jayesh1022@gmail.com>
2018-09-04 19:11:55 -0400

    Assignment 1 Completed: Added getDate function

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "Get Time",
           "Exit"
         ]
       }
@@ -22,6 +23,9 @@ module.exports = class MenuController {
       switch (response.mainMenuChoice) {
         case "Add new contact":
           this.addContact();
+          break;
+        case "Get Time":
+          this.getDate();
           break;
         case "Exit":
           this.exit();
@@ -48,5 +52,12 @@ module.exports = class MenuController {
   exit() {
     console.log("Thanks for using AddressBloc!");
     process.exit();
+  }
+
+  getDate() {
+    this.clear();
+    const time = new Date();
+    console.log(time);
+    this.main();
   }
 }
